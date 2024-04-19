ba9333e6174403e881c89f992cbca8e4e0041cf0
Yinong He <heyinong@gl-login1.arc-ts.umich.edu>
2024-04-19 19:19:05 -0400

    Delete Reduntant Directory

--- metaworld/metaworld/envs/__init__.py
+++
@@ -1,8 +0,0 @@
-from metaworld.envs.mujoco.env_dict import (ALL_V2_ENVIRONMENTS_GOAL_HIDDEN,
-                                            ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE,
-                                            ALL_V1_ENVIRONMENTS_GOAL_HIDDEN,
-                                            ALL_V1_ENVIRONMENTS_GOAL_OBSERVABLE,
-                                            )
-
-__all__ = ['ALL_V2_ENVIRONMENTS_GOAL_HIDDEN',
-           'ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE']
